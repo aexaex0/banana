8aa25d303ab27a83f5ff342d42faf9c16171d0ee
aexaex0 <peteralexq7@gmail.com>
2025-06-12 21:08:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-document.getElementById("playBtn").addEventListener("click", () => {  
-    let audio = new Audio("banana.mp3");  
-    audio.volume = 1.0; // sets max volume  
-    audio.play();  
+document.getElementById('playBtn').addEventListener('click', function () {
+    const audio = new Audio('hangod.mp3');
+    audio.volume = 1.0; // max hangerő amit enged a böngésző
+    audio.play().catch(err => {
+        console.error('Hiba a lejátszásnál:', err);
+    });
 });
